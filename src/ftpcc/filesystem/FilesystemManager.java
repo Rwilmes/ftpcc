3fe656aef49acf07cb78d934aeba5d5bb2495be9
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2017-05-28 17:10:59 +0200

    added FilesystemManager and FtpCon. Manager classes

---
+++ src/ftpcc/filesystem/FilesystemManager.java
@@ -0,0 +1,12 @@
+package ftpcc.filesystem;
+
+/**
+ * A FilesystemManager manages the local front-end filesystem.
+ * 
+ * @author RWilmes
+ * 
+ */
+public abstract class FilesystemManager {
+
+	// TODO: code & implementation
+}
---
+++ src/ftpcc/network/FtpConnectionManager.java
@@ -0,0 +1,33 @@
+package ftpcc.network;
+
+import ftpcc.network.Connection.CONNECTION_STATUS;
+
+public class FtpConnectionManager extends ConnectionManager {
+
+	// TODO: implement!
+
+	@Override
+	protected Connection getConnection(NetworkAddress destination) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Connection connect(Connection connection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Connection disconnect(Connection connection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CONNECTION_STATUS getConnectionStatus(Connection connection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
