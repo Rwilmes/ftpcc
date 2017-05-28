ec41010cab195ac54d5edfadf91b2b137c939c26
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2017-05-28 14:57:42 +0200

    added Weblaf Library

--- src/ftpcc/gui/MainFrame.java
+++ src/ftpcc/gui/MainFrame.java
@@ -1,14 +1,19 @@
 package ftpcc.gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import ftpcc.utils.Config;
 
 public class MainFrame extends JFrame {
 
-	private JTabbedPane tabPanel;
+	private JTabbedPane tabPane;
+
+	private JPanel setupPanel;
 
 	public MainFrame() {
 		// configure
@@ -16,13 +21,24 @@ public class MainFrame extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setTitle("ftpCC GUI");
-
-		this.tabPanel = new JTabbedPane();
-		this.add(this.tabPanel);
-		this.tabPanel.addTab("test", new JButton("test"));
-
-		// set visible
+		this.setMinimumSize(Config.GUI_MAIN_FRAME_SIZE);
 		this.setVisible(true);
+
+		this.tabPane = new JTabbedPane(JTabbedPane.TOP);
+
+		this.setupPanel = new JPanel();
+		this.setupPanel.add(new JButton("test-button"));
+
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		this.add(mainPanel);
+
+		// init tabbed pane
+		tabPane = new JTabbedPane(JTabbedPane.TOP);
+		tabPane.insertTab("Setup", Config.ICON_SETTINGS, this.setupPanel, null,
+				0);
+
+		mainPanel.add(tabPane, BorderLayout.CENTER);
 
 	}
 
---
+++ src/ftpcc/utils/GUI.java
@@ -0,0 +1,12 @@
+package ftpcc.utils;
+
+import com.alee.laf.WebLookAndFeel;
+
+public class GUI {
+
+	/** Installs WebLAF. **/
+	public static void installWebLAF() {
+		WebLookAndFeel.install();
+	}
+
+}
